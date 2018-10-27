413662367f360f8e74a41b2e6b10c81d6f6b5dfa
Yuka <yuky_18@hotmail.com>
2018-10-27 15:38:12 +0100

    Added current location in header
    using openweather

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-alert('Allow MOOD to access your ocation');
+$(document).ready(function () {
+    var x = document.getElementById("cityName");
+    var APPID = '575e63ead7159ab7d93bcac3dfc90181';
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    function showPosition(position) {
+        var weather = jQuery.get('https://api.openweathermap.org/data/2.5/weather?lat=38.706506999999995&lon=-9.175317&APPID=' + APPID).then(function(data)
+        {
+            x.innerHTML = data.name;
+        });
+
+    }
+
+    function main() {
+        getLocation();
+    }
+
+    main();
+});
